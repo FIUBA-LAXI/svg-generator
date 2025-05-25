528da3740e57378cd824434c4d32c52555f9db30
Lara-Ienco <lienco@fi.uba.ar>
2025-05-25 16:26:49 -0300

    ahora si anda el lsystem

--- src/tp2/lsystem.clj
+++ src/tp2/lsystem.clj
@@ -1,12 +1,13 @@
-; Manejar las iteraciones de las reglas SOBRE el axioma
+(ns tp2.lsystem)
+
+;; Maneja las iteraciones de las reglas sobre el axioma
 (defn expand-system-l [axiom rules iterations]
-  ; la funcion recibe un axioma, reglas (un mapa) e interaciones (osea las veces q se aplican las reglas
   (loop [current axiom
          n iterations]
     (if (zero? n)
-      ; si no hay mas interaciones, devuelvo el string actual
       current
-      ; sino:
-      (let [next (apply str (mapcat #(get rules (str %) [(str %)]) current))] ;Si hay una regla, se reemplaza por su valor - si no, se mantiene el carácter original.
-        ; recursivo, pero disminuyo en una la interacion
+      (let [next (apply str
+                        (mapcat (fn [c]
+                                  (get rules (str c) [(str c)]))
+                                current))]
         (recur next (dec n))))))
