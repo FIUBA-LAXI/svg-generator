0dd198cc50bbb705ae30d8ce761995901606bc04
Lara-Ienco <lienco@fi.uba.ar>
2025-05-29 14:44:58 -0300

    Agregué los extras del color y del grosor

--- src/tp2/core.clj
+++ src/tp2/core.clj
@@ -40,44 +40,61 @@
       false)))
 
 (defn validate-args [args]
-  (if (= (count args) 3)
-    true
+  (if (< (count args) 3)
     (do
-      (println "Uso: lein run <archivo-entrada> <iteraciones> <archivo-salida>")
-      false))
-  (let [input-file (nth args 0)
-        iterations (nth args 1)
-        output-file (nth args 2)]
-    (cond
-      (not (validate-reading-file input-file))
-      false
-      (not (validate-integer iterations))
-      false
-      (not (validate-writing-file output-file))
-      false
-      :else true)))
+      (println "Uso: lein run <archivo-entrada> <iteraciones> <archivo-salida> [grosor] [color]")
+      false)
+    (let [input-file (nth args 0)
+          iterations (nth args 1)
+          output-file (nth args 2)]
+      (cond
+        (not (validate-reading-file input-file))
+        false
+        (not (validate-integer iterations))
+        false
+        (not (validate-writing-file output-file))
+        false
+        :else true))))
 
-(defn -main
-  [& args]
+(defn -main [& args]
   (if (not (validate-args args))
     (System/exit 1)
     (let [input-file (nth args 0)
           iterations (Integer/parseInt (nth args 1))
           output-file (nth args 2)
+          [grosor color] (cond ; agregué esto: si el usuario en vez de poner 5 argpone 4, veo si el 4to arg es el grosor o un color.
+                           (= (count args) 4)
+                           (let [arg4 (nth args 3)]
+                             (try
+                               [(Integer/parseInt arg4) "black"] ; si es número: [grosor, color por defecto]
+                               (catch NumberFormatException _ ; si falla la conversión
+                                 [1 arg4])))  ; lo tratocomo color y grosor = 1
+                           (>= (count args) 5) ; si hay justo 5 args
+                           (let [arg4 (nth args 3) ; este es grosor
+                                 arg5 (nth args 4)] ; este es color
+                             (try
+                               [(Integer/parseInt arg4) arg5]
+                               (catch NumberFormatException _
+                                 (do
+                                   (println "Advertencia: grosor inválido, se usará 1.")
+                                   [1 arg5]))))
+                           :else
+                           [1 "black"]) ;default, uso grosor 1 y color negro!
           system-l (parser/read-system-l input-file)
           expanded (lsystem/expand-system-l (:axiom system-l)
                                             (:rules system-l)
                                             iterations)
-      lineas (turtle/interpretar expanded (:angle system-l))]  ; con turtle
+          lineas (turtle/interpretar expanded (:angle system-l))]
       (println "Sistema L cargado:")
       (println "Ángulo:" (:angle system-l))
       (println "Axioma:" (:axiom system-l))
       (println "Reglas:" (:rules system-l))
       (println "Cadena expandida:")
       (println expanded)
-      (println "Cantidad de líneas generadas por turtle:" (count lineas))
+      (println "Cantidad de líneas generadas por turtle:" (count (:lineas lineas)))
       (let [extremos (:extremos lineas)]
         (svg/write-svg-file output-file
                             (:lineas lineas)
-                            extremos))
-      )))
+                            extremos
+                            grosor
+                            color)))))
--- src/tp2/svg.clj
+++ src/tp2/svg.clj
@@ -1,7 +1,7 @@
 (ns tp2.svg
   (:require [clojure.pprint :refer [cl-format]]))
 
-(defn generate-svg [lineas extremos]
+(defn generate-svg [lineas extremos grosor color]
   (let [padding-ratio 0.1
         {:keys [min-x max-x min-y max-y]} extremos
         view-width  (- max-x min-x)
@@ -12,7 +12,6 @@
         view-min-y (- min-y padding-y)
         view-box-width (+ view-width (* 2 padding-x))
         view-box-height (+ view-height (* 2 padding-y))
-        ;; Convertimos las líneas en comandos de path:
         path-str (apply str
                         (map (fn [[[x1 y1] [x2 y2]]]
                                (str "M " (cl-format nil "~,4F" x1) " " (cl-format nil "~,4F" y1)
@@ -24,9 +23,9 @@
          view-box-width " "
          view-box-height
          "\" xmlns=\"http://www.w3.org/2000/svg\">"
-         "<path d=\"" path-str "\" stroke-width=\"1\" stroke=\"black\" fill=\"none\"/>"
+         "<path d=\"" path-str "\" stroke-width=\"" grosor "\" stroke=\"" color "\" fill=\"none\"/>"
          "</svg>")))
 
-(defn write-svg-file [file-path lineas extremos]
-  (let [svg-content (generate-svg lineas extremos)]
+(defn write-svg-file [file-path lineas extremos grosor color]
+  (let [svg-content (generate-svg lineas extremos grosor color)]
     (spit file-path svg-content)))
