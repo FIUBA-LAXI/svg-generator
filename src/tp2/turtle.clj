53c56e5f4af02699f6e82197866b98b1dfaf21a0
Lara-Ienco <lienco@fi.uba.ar>
2025-06-07 17:38:26 -0300

    solucionado que antes se apilaban estados y no tortugas

--- src/tp2/turtle.clj
+++ src/tp2/turtle.clj
@@ -37,70 +37,70 @@
 (defn interpretar [cadena angle-left angle-right]
   (loop [xs []
          figuras []
-         estado {:x 0.0 :y 0.0 :dir -90.0 :color "black" :grosor 1}
-         pila []
+         pila [{:x 0.0 :y 0.0 :dir -90.0 :color "black" :grosor 1}]
          extremos {:min-x 0.0 :max-x 0.0 :min-y 0.0 :max-y 0.0}
          [c & cs] (seq cadena)]
     (if (nil? c)
       {:lineas xs :extremos extremos :figuras figuras}
-      (case c
-        (\F \G)
-        (let [{:keys [linea estado extremos]} (mover-tortuga estado extremos true)]
-          (recur (conj xs linea) figuras estado pila extremos cs))
+      (let [estado (peek pila)]
+        (case c
+          (\F \G)
+          (let [{:keys [linea estado extremos]} (mover-tortuga estado extremos true)]
+            (recur (conj xs linea) figuras (conj (pop pila) estado) extremos cs))
 
-        (\f \g)
-        (let [{:keys [estado extremos]} (mover-tortuga estado extremos false)]
-          (recur xs figuras estado pila extremos cs))
+          (\f \g)
+          (let [{:keys [estado extremos]} (mover-tortuga estado extremos false)]
+            (recur xs figuras (conj (pop pila) estado) extremos cs))
 
-        \+
-        (recur xs figuras (update estado :dir #(+ % angle-right)) pila extremos cs)
+          \+
+          (recur xs figuras (conj (pop pila) (update estado :dir #(+ % angle-right))) extremos cs)
 
-        \-
-        (recur xs figuras (update estado :dir #(- % angle-left)) pila extremos cs)
+          \-
+          (recur xs figuras (conj (pop pila) (update estado :dir #(- % angle-left))) extremos cs)
 
-        \|
-        (recur xs figuras (update estado :dir #(+ % 180)) pila extremos cs)
+          \|
+          (recur xs figuras (conj (pop pila) (update estado :dir #(+ % 180))) extremos cs)
 
-        \[
-        (recur xs figuras estado (conj pila estado) extremos cs)
+          \[
+          (recur xs figuras (conj pila estado) extremos cs)
 
-        \]
-        (recur xs figuras (peek pila) (pop pila) extremos cs)
+          \]
+          (recur xs figuras (pop pila) extremos cs)
 
-        \C
-        (recur xs (conj figuras {:tipo :circulo :x (:x estado) :y (:y estado) :color (:color estado)}) estado pila extremos cs)
+          \C
+          (recur xs (conj figuras {:tipo :circulo :x (:x estado) :y (:y estado) :color (:color estado)}) pila extremos cs)
 
-        \R
-        (recur xs (conj figuras {:tipo :cuadrado :x (:x estado) :y (:y estado) :color (:color estado) :angulo (:dir estado)}) estado pila extremos cs)
+          \R
+          (recur xs (conj figuras {:tipo :cuadrado :x (:x estado) :y (:y estado) :color (:color estado) :angulo (:dir estado)}) pila extremos cs)
 
-        ;; cambiar color (ejemplos para a,b,c...)
-        \a (recur xs figuras (assoc estado :color "blue") pila extremos cs)
-        \b (recur xs figuras (assoc estado :color "red") pila extremos cs)
-        \c (recur xs figuras (assoc estado :color "green") pila extremos cs)
-        \d (recur xs figuras (assoc estado :color "yellow") pila extremos cs)
-        \e (recur xs figuras (assoc estado :color "purple") pila extremos cs)
-        \h (recur xs figuras (assoc estado :color "brown") pila extremos cs)
-        \i (recur xs figuras (assoc estado :color "cyan") pila extremos cs)
-        \j (recur xs figuras (assoc estado :color "magenta") pila extremos cs)
-        \k (recur xs figuras (assoc estado :color "black") pila extremos cs)
-        \l (recur xs figuras (assoc estado :color "gray") pila extremos cs)
-        \m (recur xs figuras (assoc estado :color "lime") pila extremos cs)
-        \n (recur xs figuras (assoc estado :color "navy") pila extremos cs)
-        \o (recur xs figuras (assoc estado :color "olive") pila extremos cs)
-        \p (recur xs figuras (assoc estado :color "pink") pila extremos cs)
-        \q (recur xs figuras (assoc estado :color "orange") pila extremos cs)
+          ;; cambiar color
+          \a (recur xs figuras (conj (pop pila) (assoc estado :color "blue")) extremos cs)
+          \b (recur xs figuras (conj (pop pila) (assoc estado :color "red")) extremos cs)
+          \c (recur xs figuras (conj (pop pila) (assoc estado :color "green")) extremos cs)
+          \d (recur xs figuras (conj (pop pila) (assoc estado :color "yellow")) extremos cs)
+          \e (recur xs figuras (conj (pop pila) (assoc estado :color "purple")) extremos cs)
+          \h (recur xs figuras (conj (pop pila) (assoc estado :color "brown")) extremos cs)
+          \i (recur xs figuras (conj (pop pila) (assoc estado :color "cyan")) extremos cs)
+          \j (recur xs figuras (conj (pop pila) (assoc estado :color "magenta")) extremos cs)
+          \k (recur xs figuras (conj (pop pila) (assoc estado :color "black")) extremos cs)
+          \l (recur xs figuras (conj (pop pila) (assoc estado :color "gray")) extremos cs)
+          \m (recur xs figuras (conj (pop pila) (assoc estado :color "lime")) extremos cs)
+          \n (recur xs figuras (conj (pop pila) (assoc estado :color "navy")) extremos cs)
+          \o (recur xs figuras (conj (pop pila) (assoc estado :color "olive")) extremos cs)
+          \p (recur xs figuras (conj (pop pila) (assoc estado :color "pink")) extremos cs)
+          \q (recur xs figuras (conj (pop pila) (assoc estado :color "orange")) extremos cs)
 
-        ;; cambiar grosor
-        \0 (recur xs figuras (assoc estado :grosor 0) pila extremos cs)
-        \1 (recur xs figuras (assoc estado :grosor 0.2) pila extremos cs)
-        \2 (recur xs figuras (assoc estado :grosor 0.4) pila extremos cs)
-        \3 (recur xs figuras (assoc estado :grosor 0.6) pila extremos cs)
-        \4 (recur xs figuras (assoc estado :grosor 0.8) pila extremos cs)
-        \5 (recur xs figuras (assoc estado :grosor 1) pila extremos cs)
-        \6 (recur xs figuras (assoc estado :grosor 1.2) pila extremos cs)
-        \7 (recur xs figuras (assoc estado :grosor 1.4) pila extremos cs)
-        \8 (recur xs figuras (assoc estado :grosor 1.6) pila extremos cs)
-        \9 (recur xs figuras (assoc estado :grosor 1.8) pila extremos cs)
+          ;; cambiar grosor
+          \0 (recur xs figuras (conj (pop pila) (assoc estado :grosor 0)) extremos cs)
+          \1 (recur xs figuras (conj (pop pila) (assoc estado :grosor 0.2)) extremos cs)
+          \2 (recur xs figuras (conj (pop pila) (assoc estado :grosor 0.4)) extremos cs)
+          \3 (recur xs figuras (conj (pop pila) (assoc estado :grosor 0.6)) extremos cs)
+          \4 (recur xs figuras (conj (pop pila) (assoc estado :grosor 0.8)) extremos cs)
+          \5 (recur xs figuras (conj (pop pila) (assoc estado :grosor 1)) extremos cs)
+          \6 (recur xs figuras (conj (pop pila) (assoc estado :grosor 1.2)) extremos cs)
+          \7 (recur xs figuras (conj (pop pila) (assoc estado :grosor 1.4)) extremos cs)
+          \8 (recur xs figuras (conj (pop pila) (assoc estado :grosor 1.6)) extremos cs)
+          \9 (recur xs figuras (conj (pop pila) (assoc estado :grosor 1.8)) extremos cs)
 
-        ;; Ignorar caracteres no reconocidos
-        (recur xs figuras estado pila extremos cs)))))
+          ;; Ignorar caracteres no reconocidos
+          (recur xs figuras pila extremos cs))))))
